4dedd07e8488eea312fb28b57dbc4d6fb4132517
Pascal Bayer <pascal.bayer@cubesoft.org>
2022-07-09 17:49:37 +0200

    fix(misc): update package versions

--- packages/nx-sls/src/utils/versions.ts
+++ packages/nx-sls/src/utils/versions.ts
@@ -1,6 +1,6 @@
-export const serverlessVersion = '^3.2.0';
-export const serverlessOfflineVersion = '^8.4.0';
-export const awsLambdaTypesVersion = '^8.10.85';
-export const esbuildVersion = '^0.14.21';
-export const fastGlobVersion = '^3.2.7';
+export const serverlessVersion = '^3.17.0';
+export const serverlessOfflineVersion = '^8.7.0';
+export const awsLambdaTypesVersion = '^8.10.97';
+export const esbuildVersion = '^0.14.39';
+export const fastGlobVersion = '^3.2.11';
 export const depcheckVersion = '^1.4.3';
