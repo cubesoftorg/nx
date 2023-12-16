9ec6e53977f51818707d1c7cf626ee1471dbb123
Pascal Bayer <pascal.bayer@cubesoft.org>
2023-12-16 12:23:46 +0100

    feat(repo): add unpublishing script

---
+++ scripts/unpublish-package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PWDDIR="$(pwd)"
+
+if [ -z "$1" ]
+then
+    echo "No package specified"
+    exit 1
+else
+    PACKAGE=$1
+fi
+
+cd $BASEDIR/../dist/packages/$PACKAGE
+npm unpublish --force
+
+cd $PWDDIR
